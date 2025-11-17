85c8b9f5624eba560bdf70a5ae4b0ed9359da45a
신민석 <minseok.shin@trillionlabs.co>
2025-11-17 11:28:31 +0900

    Project Init

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
+import {
+  SafeAreaProvider,
+  SafeAreaView,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <AppContent />
+    </SafeAreaProvider>
+  );
+}
+
+function AppContent() {
+  const safeAreaInsets = useSafeAreaInsets();
+
+  return (
+    <SafeAreaView style={styles.container}>
+            <Text style={{fontSize: 24}}>Hello RN!</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
