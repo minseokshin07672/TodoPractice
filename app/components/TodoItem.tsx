a9192b81f98eef1f52afe62fcd38f71f3f134b0c
신민석 <minseok.shin@trillionlabs.co>
2025-11-17 19:04:14 +0900

    add TodoItem

--- app/components/TodoItem.tsx
+++ app/components/TodoItem.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Text, StyleSheet, Pressable } from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo';
+import theme from '../styles/theme';
 
 type TodoItemStatus = 'todo' | 'done';
 
@@ -7,33 +9,53 @@ interface TodoItemProps {
     text: string;
     status: TodoItemStatus;
     onPress: () => void;
+    onIconPress?: () => void;
 }
 
-export default function TodoItem({ text, status, onPress }: TodoItemProps) {
-    const prefix = status === 'done' ? 'v ' : 'ㅁ ';
+export default function TodoItem({ text, status, onPress, onIconPress }: TodoItemProps) {
+    const prefix = status === 'done' ? '✓ ' : '□ ';
+
+    const backgroundColor =
+        status === 'done'
+            ? theme.borderGrey
+            : theme.subLightGrey;
+
+    const textColor =
+        status === 'done'
+            ? theme.subDarkGrey
+            : theme.mainDarkGrey;
+
 
     return (
-        <Pressable onPress={onPress}>
-            <Text style={[
-                styles.todoText,
-                status === 'done' && styles.doneText
-            ]}>
-                {prefix}{text}
-            </Text>
-        </Pressable>
+        <View style={[styles.container, { backgroundColor }]}>
+            <Pressable style={{ flex: 1 }} onPress={onPress}>
+                <Text style={[styles.todoText, { color: textColor }]}>
+                    {prefix}{text}
+                </Text>
+            </Pressable>
+
+            <Pressable onPress={onIconPress}>
+                <Icon
+                    name="dots-three-horizontal"
+                    size={20}
+                    color={textColor}
+                />
+            </Pressable>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
-    todoText: {
-        fontSize: 16,
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
         padding: 12,
-        backgroundColor: '#f5f5f5',
         marginBottom: 8,
         borderRadius: 8,
     },
-    doneText: {
-        backgroundColor: '#333',
-        color: '#fff',
+    todoText: {
+        fontSize: theme.fontSmall,
     },
 });
